29bf56a7828bae39a3a3053a019e1296d1c8b655
Dzmitry Misiuk <minsler.by@gmail.com>
2017-01-04 03:16:10 +0300

    Small customization of tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import * as ReactDOM from "react-dom";
 import { Hello } from "./components/Hello";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <Hello compiler="TypeScript" framework="React" version="0.1"/>,
     document.getElementById("example")
 );
