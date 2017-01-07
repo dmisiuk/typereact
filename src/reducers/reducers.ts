45b8f459ac91a74253bbdb89469938c140f31add
Dzmitry Misiuk <minsler.by@gmail.com>
2017-01-07 16:18:16 +0300

    used update instead of set in immutable map

--- src/reducers/reducers.ts
+++ src/reducers/reducers.ts
@@ -6,13 +6,14 @@ import {Action} from "../actions/actions";
 import {INCREMENT, DECREMENT} from "../actions/action_types"
 import * as Immutable from "immutable";
 
-var initialState = Immutable.Map({counter: 0});
+var initialState = Immutable.Map({counter: 0, initial: 0});
+
 export function count(state: Immutable.Map<string, any> = initialState, action: Action<number>) {
     switch (action.type) {
         case INCREMENT:
-            return state.set("counter", state.get("counter") + action.payload);
+            return state.update("counter", n => n + action.payload);
         case DECREMENT:
-            return state.set("counter", state.get("counter") - action.payload);
+            return state.update("counter", n => n - action.payload);
         default:
             return state
     }
