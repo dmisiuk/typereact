b38f47d09be4919bc5efb14fc0e7ddca21b2c53d
Dzmitry Misiuk <minsler.by@gmail.com>
2017-01-07 12:46:01 +0300

    added redux to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,36 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {Store, createStore} from "redux";
+import {Hello} from "./components/Hello";
 
-import { Hello } from "./components/Hello";
+interface Action<T> {
+    type: string;
+    payload: T;
+    error?: boolean;
+    meta?: any;
+}
+
+let devtools: any = window['devToolsExtension'] ? window['devToolsExtension']() : (f: any) => f;
+const store: Store<any> = devtools(createStore)(counter);
+
+function counter(state = 0, action: Action<number>) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + action.payload;
+        case 'DECREMENT':
+            return state - action.payload;
+        default:
+            return state
+    }
+}
+
+store.subscribe(() =>
+    console.log(store.getState())
+);
+
+store.dispatch({type: 'INCREMENT', payload: 2});
+store.dispatch({type: 'INCREMENT', payload: 3});
+
 
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" version="0.1"/>,
